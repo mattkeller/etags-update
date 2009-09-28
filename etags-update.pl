32b1e9f0ad1415e62928e2d6b548f6c4bd18f3af
Matt Keller <mk@littleredbat.net>
2009-09-28 15:43:11 -0400

    Added license on etags-update.pl

--- etags-update.pl
+++ etags-update.pl
@@ -1,6 +1,23 @@
 #!/usr/bin/perl -w
 use strict;
 
+# Copyright (C) 2009  Matt Keller <mattkeller at gmail dot com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
 # etags-update.pl: Updates a TAGS file for a given set of files.
 #
 # Requires etags in PATH.
